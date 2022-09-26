c630e9136ecc07aeb04817a9baacd4566514d49e
Kevin Gilland <kgilland@caci.com>
2022-09-26 06:43:34 -0600

    ci: porting of BandLettersRange.java

--- src/features/GridLine.ts
+++ src/features/GridLine.ts
@@ -1,111 +1,105 @@
-import { Line, Point } from "@ngageoint/grid-js";
+import { Line, Point } from '@ngageoint/grid-js';
 
 /**
  * Line between two points
- * 
+ *
  * @author osbornb
  */
 export class GridLine extends Line {
+  /**
+   * Serial Version UID
+   */
+  private static readonly serialVersionUID = 1;
 
-    /**
-     * Serial Version UID
-     */
-    private static readonly serialVersionUID = 1;
+  /**
+   * Grid type the line represents if any
+   */
+  private gridType: GridType;
 
-    /**
-     * Grid type the line represents if any
-     */
-    private gridType: GridType;
+  /**
+   * Create a line
+   *
+   * @param point1
+   *            first point
+   * @param point2
+   *            second point
+   * @param gridType
+   *            line grid type (optional)
+   * @return line
+   */
+  public static line(point1: Point, point2: Point, gridType?: GridType): GridLine {
+    const gl: GridLine = new GridLine();
+    gl.setPoints(point1, point2);
+    gl.setGridType(gridType);
+    return gl;
+  }
 
-    /**
-     * Create a line
-     * 
-     * @param point1
-     *            first point
-     * @param point2
-     *            second point
-     * @param gridType
-     *            line grid type (optional)
-     * @return line
-     */
-    public static line(point1: Point, point2: Point, gridType?: GridType): GridLine {
-        const gl: GridLine = new GridLine();
-        gl.setPoints(point1, point2);
-        gl.setGridType(gridType);
-        return gl;
+  /**
+   * Create a line
+   *
+   * @param line
+   *            line to copy
+   * @param gridType
+   *            line grid type (optional)
+   * @return line
+   */
+  public static lineFromLine(line: Line, gridType?: GridType): GridLine {
+    const gl: GridLine = new GridLine();
+    gl.hasZ = line.hasZ;
+    gl.hasM = line.hasM;
+    for (let point of line.points) {
+      gl.addPoint(point.copy() as Point);
     }
+    gl.setGridType(gridType);
+    return gl;
+  }
 
-    /**
-     * Create a line
-     * 
-     * @param line
-     *            line to copy
-     * @param gridType
-     *            line grid type (optional)
-     * @return line
-     */
-    public static lineFromLine(line: Line, gridType?: GridType): GridLine {
-        const gl: GridLine =  new GridLine();
-        gl.hasZ = line.hasZ;
-        gl.hasM = line.hasM;
-        for (let point of line.points) {
-			gl.addPoint(point.copy() as Point);
-		}
-        gl.setGridType(gridType);
-        return gl;
-    }
+  /**
+   * Get the line grid type
+   *
+   * @return grid type
+   */
+  public getGridType(): GridType {
+    return this.gridType;
+  }
 
-    /**
-     * Get the line grid type
-     * 
-     * @return grid type
-     */
-    public getGridType(): GridType {
-        return this.gridType;
-    }
+  /**
+   * Check if the line has a grid type
+   *
+   * @return true if has grid type
+   */
+  public hasGridType(): boolean {
+    return this.gridType !== null;
+  }
 
-    /**
-     * Check if the line has a grid type
-     * 
-     * @return true if has grid type
-     */
-    public hasGridType(): boolean {
-        return this.gridType !== null;
-    }
+  /**
+   * Set the line grid type
+   *
+   * @param gridType
+   *            grid type
+   */
+  public setGridType(gridType?: GridType): void {
+    this.gridType = gridType;
+  }
 
-    /**
-     * Set the line grid type
-     * 
-     * @param gridType
-     *            grid type
-     */
-    public setGridType(gridType?: GridType): void {
-        this.gridType = gridType;
-    }
+  /**
+   * Copy the line
+   *
+   * @return line copy
+   */
+  public copy(): GridLine {
+    return new GridLine(this);
+  }
 
-    /**
-     * Copy the line
-     * 
-     * @return line copy
-     */
-    public copy(): GridLine {
-        return new GridLine(this);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public equals(obj: any): boolean {
-        if (this === obj)
-            return true;
-        if (!super.equals(obj as any))
-            return false;
-        if (typeof this !== typeof obj)
-            return false;
-        const other = obj as GridLine;
-        if (this.gridType !== other.gridType)
-            return false;
-        return true;
-    }
-
+  /**
+   * {@inheritDoc}
+   */
+  public equals(obj: any): boolean {
+    if (this === obj) return true;
+    if (!super.equals(obj as any)) return false;
+    if (typeof this !== typeof obj) return false;
+    const other = obj as GridLine;
+    if (this.gridType !== other.gridType) return false;
+    return true;
+  }
 }
