24cb1d6f1c65facdd488728db14e37ac38cb1382
Kevin Gilland <kgilland@caci.com>
2022-09-27 11:59:09 -0600

    test: adding stub of a test

---
+++ test/GARSUtilsTest.ts
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import { GARSUtils } from '../src/GARSUtils';
+
+describe('GARSUtils Tests', function () {
+  it('test band value', function () {
+    expect(GARSUtils.bandValue('A')).to.equal(1);
+    expect(GARSUtils.bandValue('Z')).to.equal(24);
+    expect(GARSUtils.bandValue('AA')).to.equal(1);
+    expect(GARSUtils.bandValue('QZ')).to.equal(360);
+  });
+});
