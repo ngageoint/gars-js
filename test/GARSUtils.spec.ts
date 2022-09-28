456ba43a455af4eafc7646689d5d337a6c1be828
Kevin Gilland <kgilland@caci.com>
2022-09-28 08:44:25 -0600

    test: getting quadrants tested

--- test/GARSUtils.spec.ts
+++ test/GARSUtils.spec.ts
@@ -18,12 +18,22 @@ describe('GARSUtils Tests', function () {
    */
   it('test band letters', function () {
     expect(GARSUtils.bandLetter(1)).to.equal('A');
-    expect('Z', GARSUtils.bandLetter(24)).to.equal('Z');
+    expect(GARSUtils.bandLetter(24)).to.equal('Z');
     expect(GARSUtils.bandLetters(1)).to.equal('AA');
     expect(GARSUtils.bandLetters(24)).to.equal('AZ');
     expect(GARSUtils.bandLetters(25)).to.equal('BA');
     expect(GARSUtils.bandLetters(336)).to.equal('PZ');
     expect(GARSUtils.bandLetters(337)).to.equal('QA');
     expect(GARSUtils.bandLetters(360)).to.equal('QZ');
+  });
+
+  /**
+	 * Test quadrants
+	 */
+   it('test quadrants', function () {
+    expect(GARSUtils.quadrant(0, 1)).to.equal(1);
+		expect(GARSUtils.quadrant(1, 1)).to.equal(2);
+		expect(GARSUtils.quadrant(0, 0)).to.equal(3);
+		expect(GARSUtils.quadrant(1, 0)).to.equal(4);
   });
 });
